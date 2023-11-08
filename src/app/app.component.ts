1249b951a0f4770aff9a3f6d6d1d8c43393ce8f7
kinleyrabgay <fsd.rabgay@gmail.com>
2023-11-08 17:22:36 +0600

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { TodosComponent } from './todos/todos.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [RouterOutlet,TodosComponent]
 })
 export class AppComponent {
   title = 'signal-todo';
---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent {
+
+}
